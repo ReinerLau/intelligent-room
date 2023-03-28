90a2813970c414d90f5b15b25c465467236a290a
ReinerLau <lk850593913@gmail.com>
2023-03-28 17:33:45 +0800

    允许平移

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ onMounted(() => {
   const controls = new OrbitControls(camera, renderer.domElement)
   controls.target.set(0, 0.5, 0)
   controls.update()
-  controls.enablePan = false
+  // controls.enablePan = false
   controls.enableDamping = true
 
   const dracoLoader = new DRACOLoader()
